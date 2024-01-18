5f94ea7aa5d4d1e35414d46237ed171f16b1b3b2
junistormoen <juni.stormoen@icloud.com>
2024-01-18 14:35:14 +0100

    Fiksa feil

---
+++ app.mjs
@@ -0,0 +1,17 @@
+import chalk from "chalk";
+
+const joke = "Hvor lærte haien engelsk? På Hai School!";
+const jokeWords = joke.split(" ");
+console.log(jokeWords);
+
+const colors = [chalk.red, chalk.green, chalk.yellow, chalk.blue, chalk.magenta, chalk.cyan];
+let coloredJoke = "";
+
+
+jokeWords.forEach((word) =>{
+    const colorIndex = Math.floor(Math.random() * colors.length);
+    coloredJoke += colors[colorIndex](word) + " ";
+});
+
+console.log(coloredJoke);
+
